c0bc7ceb1d0f52d4ec5dd71dc9cfbcc5c568b2ff
Lukasz Sakowicz <lukasz.sakowicz@nvt.pl>
2019-08-29 16:01:57 +0200

    Change Makefile

--- src/lsockets.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "Address.h"
-#include "Socket.h"
-#include "NetworkUtils.h"
-#include "TcpServer.h"
-#include "Client.h"
---
+++ src/socknano.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Socket.h"
+#include "Address.h"
+#include "NetworkUtils.h"
+#include "TcpServer.h"
+#include "UdpServer.h"
+#include "TcpConnectionHandler.h"
+#include "UdpServer.h"
+#include "UdpDatagramHandler.h"
+#include "ThreadPool.h"
